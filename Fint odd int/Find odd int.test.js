d33d5e8925cd8eed2434601cc0b5bc43d2219e54
chunchia.liang <chunchia.liang61@titansoft.com.sg>
2023-02-11 20:50:19 +0800

    change description of unit test

--- Fint odd int/Find odd int.test.js
+++ Fint odd int/Find odd int.test.js
@@ -1,11 +1,11 @@
 const findOdd = require('./Find odd int');
-describe('complementDNAStrand', () => {
+describe('findOdd', () => {
     test.each([
         [[1,0,1], "0"],
         [[1,2,2,3,3,3,4,3,3,3,2,2,1], "4"],
         [[7],"7"],
         [[0],"0"],
-    ])("Nucleic acid sequence should be marked complement", (inputArray, expected)=>{
+    ])("findOdd should return occur time is odd's int", (inputArray, expected)=>{
         expect(findOdd(inputArray)).toBe(expected);
     });
   });
